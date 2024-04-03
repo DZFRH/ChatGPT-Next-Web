b435faf1b45d6cc3eba9fdcf89dbebdc9a2341fe
岱宗夫如何？ <86153432+DZFRH@users.noreply.github.com>
2024-04-03 13:30:52 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -217,7 +217,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
+            <a href={https://www.senluowanxiang.cn/shop/82.html} target="_blank" rel="noopener noreferrer">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
