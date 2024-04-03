8053419837bf6302b6a78bea1c81aec5bea7a751
岱宗夫如何？ <86153432+DZFRH@users.noreply.github.com>
2024-04-03 13:21:32 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
   title: "慧语天机-带你领略科技之美",
-  description: "Take you to appreciate the beauty of technology.",
+  description: "慧语天机是一款AI聊天机器人，它能够根据上下文与你进行自然而流畅的对话，无论你是寻求建议，还是想了解某一件事情，它都能给予你满意的答案。文能写文章，武能找bug，从慧语天机开始，带你领略科技之美。",
   viewport: {
     width: "device-width",
     initialScale: 1,
