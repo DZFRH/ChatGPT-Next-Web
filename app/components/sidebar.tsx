844e6a071f068c9dfa8826fc257fe82463eec4c4
岱宗夫如何？ <86153432+DZFRH@users.noreply.github.com>
2024-04-03 13:26:25 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          慧语天机
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Take you to appreciate the beauty of technology.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
